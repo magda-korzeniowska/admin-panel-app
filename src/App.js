2f3f810ef6d931a6a1c8a1db3bcd9640daf42ea6
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-09-08 15:13:20 +0200

    Add settings icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { FiSettings } from 'react-icons/fi'; 
 
 import './App.css';
 
 const App = () => {
   return (
-    <h1 className="underline">App</h1>
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4 z-1000'>
+            <TooltipComponent content='Settings' position='Top'>
+              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray hover:text-blue-600 text-white bg-blue-600 rounded-full'>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
