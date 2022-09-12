bf0515e8bc79a08237341d993373f4c272d21f31
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-09-12 14:03:51 +0200

    Add basic files setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import { FiSettings } from 'react-icons/fi'; 
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { FiSettings } from "react-icons/fi";
 
-import './App.css';
+import { Navbar, Sidebar } from "./components";
+import { Ecommerce } from './pages';
+import "./App.css";
+
 
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
-        <div className='flex relative dark:bg-main-dark-bg'>
-          <div className='fixed right-4 bottom-4 z-1000'>
-            <TooltipComponent content='Settings' position='Top'>
-              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray hover:text-blue-600 text-white bg-blue-600 rounded-full'>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 z-1000">
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray hover:text-blue-600 text-white bg-blue-600 rounded-full"
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path='/' element={<Ecommerce />} />
+              <Route path='/ecommerce' element={<Ecommerce />} />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default App;
