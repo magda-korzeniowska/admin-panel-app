a9347ce82518bf356be319d843672c97ce7f55ed
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-10-05 13:03:45 +0200

    Add Customers page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+export { default as Customers } from './Customers';
 export { default as Ecommerce } from './Ecommerce';
 export { default as Employees } from './Employees';
 export { default as Orders } from './Orders';
