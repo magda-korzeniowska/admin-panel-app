6be850fb3d14d4657c81ab91cec7acdb40935082
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-11-23 15:22:26 +0100

    Add Pyramid chart component

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
+
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  Inject,
+  AccumulationLegend,
+  PyramidSeries,
+  AccumulationTooltip,
+  AccumulationDataLabel,
+} from '@syncfusion/ej2-react-charts';
+
+import { ChartHeader } from '../../components';
+
+import { PyramidData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Pyramid = () => {
-  return (
-    <div>Pyramid</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Pyramid
+  return (
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartHeader category='Pyramid' title='Food Comparison' />
+      <div className='w-full'>
+        <AccumulationChartComponent
+          id='pyramid-chart'
+          legendSettings={{ background: 'white' }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject
+            services={[
+              AccumulationDataLabel,
+              AccumulationTooltip,
+              PyramidSeries,
+              AccumulationLegend,
+            ]}
+          />
+          <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective
+              dataSource={PyramidData}
+              xName='x'
+              yName='y'
+              type='Pyramid'
+              gapRatio={0.05}
+              width='50%'
+              height='80%'
+              dataLabel={{
+                visible: true,
+                position: 'Inside',
+                name: 'text',
+              }}
+            ></AccumulationSeriesDirective>
+          </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Pyramid;
