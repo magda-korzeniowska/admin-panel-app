d99658d1283668ae5e24df0659db6db2097540e5
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-11-29 16:24:16 +0100

    Add onClick to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { useStateContext } from '../contexts/ContextProvider';
+
 const Button = ({ color, bgColor, borderRadius, text, size, icon, bgHoverColor, width }) => {
+  const { handleClick } = useStateContext();
   return (
     <button
       type='button'
+      onClick={() => handleClick()}
       style={{ backgroundColor: bgColor, color, borderRadius }}
       className={`text-${size} p-3 hover:drop-shadow-xl hover:bg-${bgHoverColor} w-${width}`}
     >
