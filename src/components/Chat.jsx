9fc437163962ce862e2c2202957df5bc32f66bbf
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-11-30 11:52:17 +0100

    Add Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+
+import { Button } from '.';
+import { chatData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Chat = () => {
+  const { currentColor } = useStateContext();
   return (
-    <div>Chat</div>
-  )
-}
+    <div className='fixed top-0 right-0 bg-half-transparent w-full nav-item flex items-center justify-center h-screen '>
+      <div className='bg-white dark:bg-[#42464D] p-8 rounded-lg w-96'>
+        <div className='flex justify-between items-center'>
+          <p className='font-semibold text-lg'>Messages</p>
+          <Button
+            color='rgb(153, 171, 180)'
+            icon={<MdOutlineCancel />}
+            bgHoverColor='light-gray'
+            size='2xl'
+            borderRadius='50%'
+          />
+        </div>
+        {chatData.map((item, index) => (
+          <div
+            key={`chatData-${index}`}
+            className='flex items-center leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4'
+          >
+            <img src={item.image} alt='' className='rounded-full h-14 w-14' />
+            <div>
+              <p className='font-semibold dark:text-gray-200'>{item.message}</p>
+              <p className='text-gray-500 dark:text-gray-400 text-sm'>
+                {item.desc}
+              </p>
+              <p className='text-gray-500 dark:text-gray-400 text-xs'>
+                {item.time}
+              </p>
+            </div>
+          </div>
+        ))}
+        <div className='mt-5'>
+          <Button
+            color='white'
+            bgColor={currentColor}
+            text='See All Messages'
+            borderRadius='10px'
+            width='full'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
--- src/data/dummy.js
+++ src/data/dummy.js
@@ -5621,3 +5621,30 @@ export const cartData = [
     price: '$190',
   },
 ];
+
+export const chatData = [
+  {
+    image: avatar2,
+    message: 'Roman Joined the Team!',
+    desc: 'Congratulate him',
+    time: '9:08 AM',
+  },
+  {
+    image: avatar3,
+    message: 'New message received',
+    desc: 'Salma sent you new message',
+    time: '11:56 AM',
+  },
+  {
+    image: avatar4,
+    message: 'New Payment received',
+    desc: 'Check your earnings',
+    time: '4:39 AM',
+  },
+  {
+    image: avatar,
+    message: 'Jolly completed tasks',
+    desc: 'Assign her new tasks',
+    time: '1:12 AM',
+  },
+];
